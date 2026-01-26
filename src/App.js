26667aeeac5a33e109cb92b87acac613dce405c0
Dasom <ektha5032@gmail.com>
2026-01-26 15:46:40 +0900

    feat: add error page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import AuthCallback from "./pages/AuthCallback";
 import Pricing from "./pages/Pricing";
 import Terms from './pages/Terms';
 import Term from './components/Term';
+import ErrorPage from './pages/ErrorPage'; // ✅ ErrorPage import 추가
 import ProtectedRoute from './components/ProtectedRoute';
 import "./App.css";
 
@@ -40,6 +41,9 @@ function AppContent({ isLoggedIn, setIsLoggedIn }) {
         <Route path="/terms" element={<Terms />} />
         <Route path="/terms/:type" element={<Term />} />
 
+        {/* 🎨 CSS 작업용 테스트 라우트 */}
+        <Route path="/test-error" element={<ErrorPage />} />
+
         {/* 보호된 라우트들 */}
         <Route path="/deploy" element={
           <ProtectedRoute>
@@ -66,6 +70,9 @@ function AppContent({ isLoggedIn, setIsLoggedIn }) {
             <Pricing />
           </ProtectedRoute>
         } />
+
+        {/* 🚨 404 페이지 - 맨 마지막에! */}
+        <Route path="*" element={<ErrorPage type="404" />} />
       </Routes>
       <Footer />
     </div>
